c5d2d31463fbac7200388e3b61b8afa1212aab5c
zsinx6 <lucassin_x@hotmail.com>
2018-11-27 11:52:42 -0200

    Add more methods and errors

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 
 from flask import Flask, request, jsonify  # noqa
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse, abort
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -38,6 +38,9 @@ class ItemType(db.Model):
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
+    adress = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=False, unique=True)
+    __table_args__ = (db.UniqueConstraint('name', 'city', name='_name_city_uc'),)
 
 
 class Item(db.Model):
@@ -63,7 +66,7 @@ def resetdb():
     db.create_all()
 
 
-class get_all_item_type(Resource):
+class get_all_item_type_brand(Resource):
     """SELECT i.name, i.description, i.brand_id, b.name
     FROM item_type i JOIN brand b ON i.brand_id = b.id;
 
@@ -87,6 +90,59 @@ class get_all_item_type(Resource):
         return jsonify(json_send)
 
 
-api.add_resource(get_all_item_type, "/item_type")
+class item_type_id(Resource):
+    def get(self, _id):
+        query = (db.session.query(ItemType.name,
+                                  ItemType.description,
+                                  ItemType.brand_id,
+                                  ItemType.id)
+                 .filter(ItemType.id == _id).first())
+
+        json_send = {}
+        json_send[query.id] = {"name": query.name,
+                               "description": query.description,
+                               "brand_id": query.brand_id}
+        return jsonify(json_send)
+
+    def delete(self, _id):
+        pass
+
+    def put(self, _id):
+        pass
+
+    def patch(self, _id):
+        pass
+
+
+class new_item_type(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("name", type=str)
+    parser.add_argument("description", type=str)
+    parser.add_argument("brand_id", type=int)
+
+    def post(self):
+        document = self.parser.parse_args(strict=True)
+        name = document.get("name")
+        description = document.get("description")
+        brand_id = document.get("brand_id")
+
+        query_brand = (db.session.query(Brand.id,
+                                        Brand.name)
+                       .filter(Brand.id == brand_id).first())
+        if not query_brand:
+            abort(404, message="brand_id {} doesn't exists".format(brand_id))
+
+        item_type = ItemType(name=name, description=description, brand_id=brand_id)
+        db.session.add(item_type)
+        db.session.commit()
+
+        json_send = {}
+        json_send[item_type.id] = {"name": name, "description": description, "brand_id": brand_id}
+        return jsonify(json_send)
+
+
+api.add_resource(get_all_item_type_brand, "/all_item_type_brand")
+api.add_resource(item_type_id, "/id_item_type/<_id>")
+api.add_resource(new_item_type, "/create_item_type")
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
